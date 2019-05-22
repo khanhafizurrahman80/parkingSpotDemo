e2380c8acefefc8c6b885922453e4ca5c99745c5
khanhafizurrahman80 <khanhafizurrahman80@gmail.com>
2019-05-22 23:49:01 +0200

    Login View Added

---
+++ parking_kothai/View/LoginView.swift
@@ -0,0 +1,13 @@
+//
+//  LoginView.swift
+//  parking_kothai
+//
+//  Created by Khan hafizur rahman on 22/5/19.
+//  Copyright © 2019 Khan hafizur rahman. All rights reserved.
+//
+
+import UIKit
+
+class LoginView: UIView {
+    
+}
